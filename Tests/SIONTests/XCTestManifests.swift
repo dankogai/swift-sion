0838ad47e620a3305414db72e0f6d76e1408be11
Dan Kogai <dankogai+github@gmail.com>
2018-09-21 19:49:27 +0900

    Linux fixes (1 typo)

--- Tests/SIONTests/XCTestManifests.swift
+++ Tests/SIONTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(JSONTests.allTests),
+        testCase(SIONTests.allTests),
     ]
 }
 #endif
