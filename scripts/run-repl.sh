45a9b709a3cc624701e531915fb13b567eeb2b7f
Dan Kogai <dankogai+github@gmail.com>
2018-09-21 23:56:54 +0900

    simply run-repl.sh

--- scripts/run-repl.sh
+++ scripts/run-repl.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    inc=-I/usr/lib/swift/clang/include
-else
-    inc=""
-fi
 bd=".build/release"
 swift build -c release -Xswiftc -enable-testing \
-    && swift ${inc} -I${bd} -L${bd} -lSION
+    && swift -I${bd} -L${bd} -lSION
